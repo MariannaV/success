a85b3f66999ed5e09377f7375c3a66c2adb0ba38
Marianna <35933634+MariannaV@users.noreply.github.com>
2020-01-13 00:03:32 +0300

    Страница "Карточка товара" (#10)

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * [Главная](https://my-success.netlify.com/main-page.html)
 * [Акт сверки](https://my-success.netlify.com/act-page.html) ([1я версия](https://my-success.netlify.com/act-page-2.html))
 * [Прайс-лист](https://my-success.netlify.com/price-page.html)
+* [Карточка товара](https://my-success.netlify.com/product-page.html)
---
+++ src/components/product-page/index.js
@@ -0,0 +1,18 @@
+const productHandlers = {
+    description: {
+        navigation: {
+            onToggleActiveTab: (event) => {
+                event.stopPropagation();
+                const {currentTarget: container, target: activeEl} = event;
+                if (activeEl.classList.contains('product-description__navigation-block')) {
+                    if (window.matchMedia("(min-width: 768px)").matches)
+                        container.querySelector('.active-tab').classList.remove('active-tab');
+                    activeEl.classList.toggle('active-tab');
+                }
+            }
+        }
+    }
+};
+
+if (window.matchMedia("(max-width: 767px)").matches) [...document.querySelectorAll(".product-description__content > input[type='radio']")
+].forEach(el => el.setAttribute("type", "checkbox"));
